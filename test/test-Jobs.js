5926f22361bedade6290285580bf90df613f3786
Kartik Thakore <kthakore@newtopia.com>
2016-10-30 13:10:30 -0400

    JS Lint

--- test/test-Jobs.js
+++ test/test-Jobs.js
@@ -1,19 +1,20 @@
-/*global require, should, describe */
 'use strict'
 var should = require('should'),
-    Jobs   = require('../lib/Jobs')
+  Jobs   = require('../lib/Jobs'),
+  debug   = require('debug')('mocha')
 
 
 describe('Jobs Class ', function () {
 
-   it('Jobs can get', function (done) {
-     var jobs = new Jobs()
+  it('Jobs can get', function (done) {
+    var jobs = new Jobs()
 
-     jobs.on('loaded', (msg) => {
+    jobs.on('loaded', (msg) => {
+      debug(msg)
+      should.exist(msg)
+      done()
+    })
+    jobs.GetData()
 
-        done()
-      })
-     jobs.GetData()
-
-   })
+  })
 })
